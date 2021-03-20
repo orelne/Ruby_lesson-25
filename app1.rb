a9e63bbc3d29897067c8abd9228268a3952ece8d
Green Alexandr <orelne7@gmail.com>
2021-03-20 19:39:24 +0300

    Add select query

--- app1.rb
+++ app1.rb
@@ -3,6 +3,10 @@ db = SQLite3::Database.new 'mycars.sqlite'
 
 db.execute "insert into Cars (name, price) values ('Jaguar','7777')"
 
+db.execute "select * from Cars" do |car|
+	puts car
+	puts "===="
+end
 db.close
 
 
